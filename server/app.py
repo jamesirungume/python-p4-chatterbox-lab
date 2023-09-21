bee3f8ba2d1144a3f0838b8373f0a592fee111d1
Your Name <jymohcooler@gmail.com>
2023-09-21 14:05:25 +0300

    Complete project

--- server/app.py
+++ server/app.py
@@ -14,13 +14,36 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()  # Fixed the typo in 'asc'
+    message_list = [{'id': message.id, 'body': message.body, 'username': message.username, 'created_at': message.created_at.isoformat()} for message in messages]
+    return jsonify(message_list), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def add_messages():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify({'id': new_message.id,'username':new_message.username, 'body': new_message.body, 'created_at': new_message.created_at.isoformat()}), 201  # Fixed the status code
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_messages(id):
+    messaged = Message.query.get(id)
+    if messaged:
+        data = request.get_json()
+        messaged.body = data.get('body')
+        db.session.commit()
+        return jsonify({'id': messaged.id, 'body': messaged.body, 'username': messaged.username, 'created_at': messaged.created_at.isoformat()}), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    messaged = Message.query.get(id)
+    if messaged:
+        db.session.delete(messaged)
+        db.session.commit()
+        return jsonify({'message': 'Message deleted'}), 200  # Fixed the response message
 
 if __name__ == '__main__':
     app.run(port=5555)
